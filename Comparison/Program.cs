dc271015a9e312d259b707c9d33aee32c1da2c33
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2018-12-12 17:37:23 -0200

    comparando dois inteiros

---
+++ Comparison/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1=0,number2=0;
+
+            Console.Write("Entre o primeiro número:");
+            number1 = Int32.Parse( Console.ReadLine() );
+            Console.Write("Entre o segundo número:");
+            number2 = Int32.Parse( Console.ReadLine() );
+            
+          if(number1 == number2)
+                Console.WriteLine(number1 + "==" + number2);
+          if(number1 != number2)
+                Console.WriteLine(number1 + "!=" + number2);
+          if(number1 > number2)
+                Console.WriteLine(number1 + ">" + number2);
+          if(number1 < number2)
+                Console.WriteLine(number1 + "<" + number2);
+          if(number1 <= number2)
+                Console.WriteLine(number1 + "<=" + number2);
+          if(number1 >= number2)
+                Console.WriteLine(number1 + ">=" + number2);
+            
+        }
+    }
+}
