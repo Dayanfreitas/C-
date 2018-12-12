544bc5d4fbce5a972b18ff50bbc543471250e3d3
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2018-12-12 16:41:56 -0200

    soma de dois numeros

---
+++ Addition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber,
+                    secondNumber;
+            int number1,
+                number2,
+                sum;
+            Console.Write("Entre com o primerio número:");
+            firstNumber = Console.ReadLine();
+            Console.Write("Entre com o segundo número:");
+            secondNumber = Console.ReadLine();
+            number1 = Int32.Parse(firstNumber);
+            number2 = Int32.Parse(secondNumber);
+            sum = number1 + number2;
+            Console.Write($"{number1} + {number2} = {sum}");            
+        }
+    }
+}
