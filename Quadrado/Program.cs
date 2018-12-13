1913d8fe8776a7022e0329f888049516a719c9bd
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2018-12-13 16:54:29 -0200

    atualizado

---
+++ Quadrado/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número\tQuadrado\tCubo");
+            for(int i=0; i <=10;i++){
+                Console.WriteLine($"{i}\t{Math.Pow(i,2)}\t\t{Math.Pow(i,3)}");
+            }
+        }
+    }
+}
