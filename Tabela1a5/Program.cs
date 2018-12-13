51118df0a5f45d60a2ad5de8f75fa35d577bea1e
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2018-12-13 17:36:34 -0200

    tabela de multiplicacao de 1 a 5

---
+++ Tabela1a5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tabela1a5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            Console.WriteLine("N\t10*N\t100*N\t1000*N");
+            while(i<=5){
+                Console.Write($"{i}\t{i*10}\t{i*100}\t{i*1000}\n");
+                i++;
+            }
+        }
+    }
+}
