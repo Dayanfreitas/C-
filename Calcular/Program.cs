98704399d58b4a3add8a3bd4202df467cda0fa67
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2018-12-13 17:25:41 -0200

    soma de 1 a 10

---
+++ Calcular/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calcular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=1;
+            int fim=10;
+            int soma=0;
+            while(x <= fim){
+                soma += x++;
+            }
+            Console.WriteLine($"Soma:{soma}");
+        }
+    }
+}
